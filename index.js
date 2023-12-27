949c115e26ad038d45d27095ebf597f56e544f83
mdmohiuddin10 <hmmohiuddinctg1997@gmail.com>
2023-12-27 10:42:21 +0600

    first typescript fuction

---
+++ index.js
@@ -0,0 +1,9 @@
+var userName = 'Mohammad Mohiuddin';
+var age = 63;
+console.log(userName, age);
+function addNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumbers(20, '30');
+// to run in command .... index.ts - tsc index.ts - index.js - node index.js
+// to run onetime = tsc index.ts --watch
---
+++ index.ts
@@ -0,0 +1,13 @@
+let userName = 'Mohammad Mohiuddin';
+let age = 63;
+console.log(userName, age);
+
+function addNumbers(num1: number, num2: number){
+    console.log(num1 + num2);
+}
+
+// addNumbers(20, '30')
+addNumbers(20, 30)
+
+// to run in command .... index.ts - tsc index.ts - index.js - node index.js
+// to run onetime = tsc index.ts --watch
