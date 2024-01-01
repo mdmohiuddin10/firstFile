f008528ba389d2bbb3a04713fcbf1e2a3eff3f09
mdmohiuddin10 <hmmohiuddinctg1997@gmail.com>
2024-01-01 09:46:02 +0600

    craete a simple code in ts

---
+++ built-in-type.js
@@ -0,0 +1,7 @@
+// built in type: number, string, boolean, void, undefine, null
+var userId;
+userId = 56;
+var userNames;
+userNames = 'mohiuddin';
+var isActivated;
+isActivated = false;
---
+++ built-in-type.ts
@@ -0,0 +1,9 @@
+// built in type: number, string, boolean, void, undefine, null
+let userId: number;
+userId = 56;
+console.log(userId);
+
+let userNames: string;
+userNames = 'mohiuddin';
+let isActivated: boolean;
+isActivated = false;
