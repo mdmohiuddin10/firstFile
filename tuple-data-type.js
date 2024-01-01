7b5c122e297dd181ee225e56a0d26777c40d45b9
mdmohiuddin10 <hmmohiuddinctg1997@gmail.com>
2024-01-01 10:32:44 +0600

    tuple-data-types

---
+++ tuple-data-type.js
@@ -0,0 +1,4 @@
+// mixed data - key, value
+var user;
+user = [102, 'anis'];
+console.log(user);
---
+++ tuple-data-type.ts
@@ -0,0 +1,4 @@
+// mixed data - key, value
+let user: [number, string];
+user = [102, 'anis']
+console.log(user);
